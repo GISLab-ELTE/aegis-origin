2cd940193c1857bd315cca3d86f2d5c3a157bcc8
groberto <groberto@inf.elte.hu>
2014-06-23 18:12:42 +0200

    AEGIS.Core.Graphs: Extended functionality of factory by including comperators.

--- AEGIS.Core.Graphs/Geometry/GeometryGraphFactory.cs
+++ AEGIS.Core.Graphs/Geometry/GeometryGraphFactory.cs
@@ -56,6 +56,30 @@ namespace ELTE.AEGIS.Geometry
         /// <summary>
         /// Creates a graph.
         /// </summary>
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>An empty graph using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer)
+        {
+            return new GeometryGraph(this, null, vertexEqualityComparer, edgeEqualityComparer);
+        }
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>An empty graph with the specified metadata using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer, IDictionary<String, Object> metadata)
+        {
+            return new GeometryGraph(this, metadata, vertexEqualityComparer, edgeEqualityComparer);
+        }
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
         /// <param name="metadata">The metadata.</param>
         /// <returns>An empty graph with the specified metadata.</returns>
         public IGeometryGraph CreateGraph(IDictionary<String, Object> metadata)
@@ -70,18 +94,32 @@ namespace ELTE.AEGIS.Geometry
         /// <returns>A graph containing the specified coordinates as vertices.</returns>
         public IGeometryGraph CreateGraph(IEnumerable<Coordinate> coordinates)
         {
-            return CreateGraph(coordinates, null);
+            return CreateGraph(coordinates, null, null, null);
         }
 
         /// <summary>
         /// Creates a graph.
         /// </summary>
         /// <param name="coordinates">The sequence of coodinates.</param>
-        /// <param name="metadata">The metadata.</param>
-        /// <returns>A graph containing the specified coordinates as vertices and the specified metadata.</returns>
-        public IGeometryGraph CreateGraph(IEnumerable<Coordinate> coordinates, IDictionary<String, Object> metadata)
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <returns>A graph containing the specified coordinates as vertices using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IEnumerable<Coordinate> coordinates, IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer)
         {
-            IGeometryGraph graph = new GeometryGraph(this, metadata);
+            return CreateGraph(coordinates, vertexEqualityComparer, edgeEqualityComparer, null);
+        }
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
+        /// <param name="coordinates">The sequence of coodinates.</param>
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>A graph containing the specified coordinates as vertices and the metadata using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IEnumerable<Coordinate> coordinates, IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer, IDictionary<String, Object> metadata)
+        {
+            IGeometryGraph graph = new GeometryGraph(this, metadata, vertexEqualityComparer, edgeEqualityComparer);
 
             if (coordinates != null)
             {
@@ -92,6 +130,18 @@ namespace ELTE.AEGIS.Geometry
             return graph;
         }
 
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
+        /// <param name="coordinates">The sequence of coodinates.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>A graph containing the specified coordinates as vertices and the specified metadata.</returns>
+        public IGeometryGraph CreateGraph(IEnumerable<Coordinate> coordinates, IDictionary<String, Object> metadata)
+        {
+            return CreateGraph(coordinates, null, null, metadata);
+        }
+
         /// <summary>
         /// Creates a graph.
         /// </summary>
@@ -99,18 +149,32 @@ namespace ELTE.AEGIS.Geometry
         /// <returns>A graph containing the specified coordinates as vertices and edges.</returns>
         public IGeometryGraph CreateGraph(IEnumerable<IEnumerable<Coordinate>> coordinates)
         {
-            return CreateGraph(coordinates, null);
+            return CreateGraph(coordinates, null, null, null);
         }
 
         /// <summary>
         /// Creates a graph.
         /// </summary>
         /// <param name="coordinates">The sequence of edge sequences.</param>
-        /// <param name="metadata">The metadata.</param>
-        /// <returns>A graph containing the specified coordinates as vertices and edges and the specified metadata.</returns>
-        public IGeometryGraph CreateGraph(IEnumerable<IEnumerable<Coordinate>> coordinates, IDictionary<String, Object> metadata)
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <returns>A graph containing the specified coordinates as vertices and edges using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IEnumerable<IEnumerable<Coordinate>> coordinates, IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer)
         {
-            IGeometryGraph graph = new GeometryGraph(this, metadata);
+            return CreateGraph(coordinates, vertexEqualityComparer, edgeEqualityComparer, null);
+        }
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
+        /// <param name="coordinates">The sequence of edge sequences.</param>
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>A graph containing the specified coordinates as vertices and edges and the metadata using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IEnumerable<IEnumerable<Coordinate>> coordinates, IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer, IDictionary<String, Object> metadata)
+        {
+            IGeometryGraph graph = new GeometryGraph(this, metadata, vertexEqualityComparer, edgeEqualityComparer);
             if (coordinates != null)
             {
                 foreach (IEnumerable<Coordinate> coordinateCollection in coordinates)
@@ -143,10 +207,37 @@ namespace ELTE.AEGIS.Geometry
         /// <summary>
         /// Creates a graph.
         /// </summary>
+        /// <param name="coordinates">The sequence of edge sequences.</param>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns>A graph containing the specified coordinates as vertices and edges and the specified metadata.</returns>
+        public IGeometryGraph CreateGraph(IEnumerable<IEnumerable<Coordinate>> coordinates, IDictionary<String, Object> metadata)
+        {
+            return CreateGraph(coordinates, null, null,  metadata);
+        }
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
         /// <returns>A graph that matches <param name="other" />.</returns>
         public IGeometryGraph CreateGraph(IGeometryGraph other)
         {
             IGeometryGraph graph = new GeometryGraph(this, other.Metadata, other.VertexComparer, other.EdgeComparer);
+
+            other.ToGraph(graph); // using the extension method
+
+            return graph;
+        }
+
+        /// <summary>
+        /// Creates a graph.
+        /// </summary>
+        /// <param name="other">The other geometry graph.</param>
+        /// <param name="vertexEqualityComparer">The vertex comparer.</param>
+        /// <param name="edgeEqualityComparer">The edge comparer.</param>
+        /// <returns>A graph that matches <param name="other" /> using the specified comparers.</returns>
+        public IGeometryGraph CreateGraph(IGeometryGraph other, IEqualityComparer<IGraphVertex> vertexEqualityComparer, IEqualityComparer<IGraphEdge> edgeEqualityComparer)
+        {
+            IGeometryGraph graph = new GeometryGraph(this, other.Metadata, vertexEqualityComparer, edgeEqualityComparer);
 
             other.ToGraph(graph); // using the extension method
 
