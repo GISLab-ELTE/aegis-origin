005b36865b066297803e47680d58f7c6c02e62b9
groberto <groberto@inf.elte.hu>
2014-07-16 16:01:15 +0200

    AEGIS.IO.Hadoop/Storage/Operation: Added boolean, file status and file statuses results (HadoopBooleanOperationResult, HadoopFileListingOperationResult, HadoopFileStatusOperationResult).

---
+++ AEGIS.IO.Hadoop/Storage/Operation/HadoopFileListingOperationResult.cs
@@ -0,0 +1,62 @@
+﻿/// <copyright file="HadoopFileListingOperationResult.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2014 Roberto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Roberto Giachetta</author>
+
+using System;
+using System.Collections.Generic;
+
+namespace ELTE.AEGIS.IO.Storage.Operation
+{
+    /// <summary>
+    /// Represents a Hadoop file system operation result containing a file status list.
+    /// </summary>
+    public class HadoopFileListingOperationResult : HadoopFileSystemOperationResult
+    {
+        #region Public properties
+
+        /// <summary>
+        /// Gets or sets the status list.
+        /// </summary>
+        /// <value>The status list.</value>
+        public IList<HadoopFileStatusOperationResult> StatusList { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HadoopFileListingOperationResult"/> class.
+        /// </summary>
+        public HadoopFileListingOperationResult() 
+        {
+            StatusList = new HadoopFileStatusOperationResult[0];
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HadoopFileListingOperationResult" /> class.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="statusList">The list of file status results.</param>
+        public HadoopFileListingOperationResult(String request, IList<HadoopFileStatusOperationResult> statusList)
+            :base(request)
+        {
+            if (statusList == null)
+                StatusList = new HadoopFileStatusOperationResult[0];
+            else
+                StatusList = statusList; 
+        }
+
+        #endregion
+    }
+}
---
+++ AEGIS.IO.Hadoop/Storage/Operation/HadoopFileStatusOperationResult.cs
@@ -0,0 +1,95 @@
+﻿/// <copyright file="HadoopFileStatusOperationResult.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2014 Roberto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Roberto Giachetta</author>
+
+using System;
+
+namespace ELTE.AEGIS.IO.Storage.Operation
+{
+    /// <summary>
+    /// Represents a Hadoop file system operation result containing a file status.
+    /// </summary>
+    public class HadoopFileStatusOperationResult : HadoopFileSystemOperationResult
+    {
+        #region Public properties
+
+        /// <summary>
+        /// Gets the name of the entry.
+        /// </summary>
+        /// <value>The name of the entry.</value>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time of the last access.
+        /// </summary>
+        /// <value>The time of the last access.</value>
+        public DateTime AccessTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time of the last modification.
+        /// </summary>
+        /// <value>The time of the last modification.</value>
+        public DateTime ModificationTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the length of the file.
+        /// </summary>
+        /// <value>The length of the file in bytes.</value>
+        public Int64 Length { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the blocks.
+        /// </summary>
+        /// <value>The size of the blocks in bytes.</value>
+        public Int64 BlockSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the entry.
+        /// </summary>
+        /// <value>The type of the entry.</value>
+        public FileSystemEntryType EntryType { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HadoopFileStatusOperationResult"/> class.
+        /// </summary>
+        public HadoopFileStatusOperationResult() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HadoopBooleanOperationResult" /> class.
+        /// </summary>
+        /// <param name="request">The request of the operation.</param>
+        /// <param name="name">The name of the entry.</param>
+        /// <param name="accessTime">The time of the last access.</param>
+        /// <param name="modificationTime">The time of the last modification.</param>
+        /// <param name="length">The length of the file.</param>
+        /// <param name="blockSize">The size of the block.</param>
+        /// <param name="entryType">the type of the entry.</param>
+        public HadoopFileStatusOperationResult(String request, String name, DateTime accessTime, DateTime modificationTime, Int64 length, Int64 blockSize, FileSystemEntryType entryType)
+            : base(request) 
+        {
+            Name = name;
+            AccessTime = accessTime;
+            ModificationTime = modificationTime;
+            Length = length;
+            BlockSize = blockSize;
+            EntryType = entryType;
+        }
+
+        #endregion
+    }
+}
