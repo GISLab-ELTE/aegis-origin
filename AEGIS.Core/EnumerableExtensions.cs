d0659a60114f92e46dbe9830c29c6d5e8ee87044
groberto <groberto@inf.elte.hu>
2014-07-24 10:32:52 +0200

    AEGIS.Core: Added read-only wrapper queries and extended collection conversion to EnumerableExtensions.

--- AEGIS.Core/EnumerableExtensions.cs
+++ AEGIS.Core/EnumerableExtensions.cs
@@ -13,17 +13,21 @@
 /// </copyright>
 /// <author>Roberto Giachetta</author>
 
+using ELTE.AEGIS.Collections;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace ELTE.AEGIS
 {
     /// <summary>
-    /// Provides extensions to geometry enumerations.
+    /// Provides extensions to enumerations.
     /// </summary>
     public static class EnumerableExtensions
     {
+        #region Public static extension methods
+
         /// <summary>
         /// Converts a geometry enumeration to geometry collection.
         /// </summary>
@@ -31,21 +35,49 @@ namespace ELTE.AEGIS
         /// <param name="collection">The collection of geometries.</param>
         /// <returns>The geometry collection containing the geometries.</returns>
         /// <exception cref="System.ArgumentNullException">The collection is null.</exception>
-        /// <exception cref="System.ArgumentException">
-        /// The collection is empty.
-        /// or
-        /// The factory of the geometries in the collection is not the same.
-        /// </exception>
+        /// <exception cref="System.ArgumentException">The factory of the geometries in the collection is not the same.</exception>
         public static IGeometryCollection<T> ToGeometryCollection<T>(this IEnumerable<T> collection) where T : IGeometry
         {
             if (collection == null)
                 throw new ArgumentNullException("collection", "The collection is null.");
+
+            // in case of empty collection the default factory is used
             if (!collection.Any())
-                throw new ArgumentException("The collection is empty.", "collection");
+                return Factory.DefaultInstance<IGeometryFactory>().CreateGeometryCollection<T>(collection);
+
+            // the factory of the 
             if (collection.Any(geometry => !geometry.Factory.Equals(collection.First().Factory)))
                 throw new ArgumentException("The factory of the geometries in the collection is not the same.", "collection");
 
             return collection.First().Factory.CreateGeometryCollection<T>(collection);
         }
+
+        /// <summary>
+        /// Returns a read-only dictionary wrapper for the current collection.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="collection">The dictionary.</param>
+        /// <returns>A <see cref="ReadOnlyDictionary{TKey, TValue}"/> that acts as a read-only wrapper around the current <see cref="IDictionary{TKey, TValue}"/>.</returns>
+        public static IDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection", "The collection is null.");
+
+            return new ReadOnlyDictionary<TKey, TValue>(collection);
+        }
+
+        /// <summary>
+        /// Returns a read-only set wrapper for the current collection.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="collection">The set.</param>
+        /// <returns>A <see cref="ReadOnlySet{T}"/> that acts as a read-only wrapper around the current <see cref="ISet{T}"/>.</returns>
+        public static ISet<T> AsReadOnly<T>(this ISet<T> collection)
+        {
+            return new ReadOnlySet<T>(collection);
+        }
+
+        #endregion
     }
 }
