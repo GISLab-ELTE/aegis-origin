38bd769e6e6a83b4888aa93c63d291f5730c6fec
groberto <groberto@inf.elte.hu>
2014-07-31 13:02:34 +0200

    AEGIS.Operations.Graphs/Spatial/ShortestPath: Added Dijkstra's algorithm for shortest path tree (DijkstrasAlgorithm). AEGIS.Tests.Operations.Graphs: Added unit test for DijkstrasAlgorithm (DijkstrasAlgorithmTest).

---
+++ AEGIS.Operations.Graphs/Spatial/ShortestPath/DijkstrasAlgorithm.cs
@@ -0,0 +1,148 @@
+﻿/// <copyright file="DijkstrasAlgorithm.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2014 Robeto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Roberto Giachetta</author>
+
+using ELTE.AEGIS.Collections;
+using ELTE.AEGIS.Operations.Management;
+using System;
+using System.Collections.Generic;
+
+namespace ELTE.AEGIS.Operations.Spatial.ShortestPath
+{
+    /// <summary>
+    /// Represents an operation performing Dijkstra's algorithm.
+    /// </summary>
+    [OperationClass("AEGIS::212310", "Dijkstra's algorithm")]
+    public class DijkstrasAlgorithm : ShortestPathTreeAlgorithm
+    {
+        #region Private fileds
+
+        /// <summary>
+        /// The priority queue used for ordering vertices.
+        /// </summary>
+        private Heap<Double, IGraphVertex> _priorityQueue;
+
+        /// <summary>
+        /// The set of finished vertices.
+        /// </summary>
+        protected HashSet<IGraphVertex> _finished;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DijkstrasAlgorithm" /> class.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// The source is null.
+        /// or
+        /// The method requires parameters which are not specified.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// The parameters do not contain a required parameter value.
+        /// or
+        /// The type of a parameter does not match the type specified by the method.
+        /// or
+        /// The value of a parameter is not within the expected range.
+        /// or
+        /// The specified source and result are the same objects, but the method does not support in-place operations.
+        /// </exception>
+        public DijkstrasAlgorithm(IGeometryGraph source, IDictionary<OperationParameter, Object> parameters)
+            : this(source, null, parameters)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DijkstrasAlgorithm" /> class.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="target">The target.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// The source is null.
+        /// or
+        /// The method requires parameters which are not specified.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// The parameters do not contain a required parameter value.
+        /// or
+        /// The type of a parameter does not match the type specified by the method.
+        /// or
+        /// The value of a parameter is not within the expected range.
+        /// or
+        /// The specified source and result are the same objects, but the method does not support in-place operations.
+        /// </exception>
+        public DijkstrasAlgorithm(IGeometryGraph source, IGeometryGraph target, IDictionary<OperationParameter, Object> parameters)
+            : base(source, target, GraphOperationMethods.DijkstrasAlgorithm, parameters)
+        {
+        }
+
+        #endregion
+
+        #region Protected Operation methods
+
+        /// <summary>
+        /// Prepares the result of the operation.
+        /// </summary>
+        protected override void PrepareResult()
+        {
+            base.PrepareResult();
+
+            _priorityQueue = new Heap<Double, IGraphVertex>();
+            _finished = new HashSet<IGraphVertex>();
+        }
+
+        /// <summary>
+        /// Computes the result of the operation.
+        /// </summary>
+        protected override void ComputeResult()
+        {
+            _priorityQueue.Insert(0, _sourceVertex);
+            _distance.Add(_sourceVertex, 0);
+            _parent.Add(_sourceVertex, null);
+
+            while (_priorityQueue.Count > 0)
+            {
+                IGraphVertex currentVertex = _priorityQueue.RemovePeek();
+
+                foreach (IGraphEdge edge in _source.OutEdges(currentVertex))
+                {
+                    if (_finished.Contains(edge.Target))
+                        continue;
+
+                    _finished.Add(currentVertex);
+
+                    if (!_distance.ContainsKey(edge.Target))
+                    {
+                        _distance[edge.Target] = _distance[currentVertex] + _weightMetric(edge);
+                        _parent[edge.Target] = currentVertex;
+                        _priorityQueue.Insert(_distance[edge.Target], edge.Target);
+                    }
+                    else if (_distance[edge.Target] > _distance[currentVertex] + _weightMetric(edge))
+                    {
+                        _distance[edge.Target] = _distance[currentVertex] + _weightMetric(edge);
+                        _parent[edge.Target] = currentVertex;
+                        _priorityQueue.Insert(_distance[edge.Target], edge.Target);
+                    }
+                }
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ AEGIS.Tests.Operations.Graphs/Spatial/ShortestPath/DijkstrasAlgorithmTest.cs
@@ -0,0 +1,197 @@
+﻿/// <copyright file="DijkstrasAlgorithmTest.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2014 Robeto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Roberto Giachetta</author>
+
+using ELTE.AEGIS.Geometry;
+using ELTE.AEGIS.Operations;
+using ELTE.AEGIS.Operations.Spatial;
+using ELTE.AEGIS.Operations.Spatial.ShortestPath;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ELTE.AEGIS.Tests.Operations.Spatial.ShortestPath
+{
+    /// <summary>
+    /// Test fixture for class <see cref="DijkstrasAlgorithm" />.
+    /// </summary>
+    public class DijkstrasAlgorithmTest
+    {
+        #region Private fields
+
+        /// <summary>
+        /// The mock of the reference system.
+        /// </summary>
+        private Mock<IReferenceSystem> _referenceSystemMock;
+
+        /// <summary>
+        /// The source graph.
+        /// </summary>
+        private IGeometryGraph _sourceGraph;
+
+        /// <summary>
+        /// The result graph.
+        /// </summary>
+        private IGeometryGraph _resultGraph;
+
+        /// <summary>
+        /// The source vertex.
+        /// </summary>
+        private IGraphVertex _sourceVertex;
+
+        /// <summary>
+        /// The target vertex.
+        /// </summary>
+        private IGraphVertex _targetVertex;
+
+
+        #endregion
+
+        #region Test setup
+
+        /// <summary>
+        /// Test setup.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            _referenceSystemMock = new Mock<IReferenceSystem>(MockBehavior.Strict);
+
+            // source: http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+            _sourceGraph = Factory.GetInstance<GeometryGraphFactory>(_referenceSystemMock.Object).CreateNetwork();
+
+            // vertices
+            IGraphVertex vertex1 = _sourceGraph.AddVertex(new Coordinate(0, 0));
+            IGraphVertex vertex2 = _sourceGraph.AddVertex(new Coordinate(1, 0));
+            IGraphVertex vertex3 = _sourceGraph.AddVertex(new Coordinate(1, 1));
+            IGraphVertex vertex4 = _sourceGraph.AddVertex(new Coordinate(2, 1));
+            IGraphVertex vertex5 = _sourceGraph.AddVertex(new Coordinate(1, 2));
+            IGraphVertex vertex6 = _sourceGraph.AddVertex(new Coordinate(0, 2));
+
+            // forward edges
+            _sourceGraph.AddEdge(vertex1, vertex2, CreateWeightMetadata(7));
+            _sourceGraph.AddEdge(vertex1, vertex3, CreateWeightMetadata(9));
+            _sourceGraph.AddEdge(vertex1, vertex6, CreateWeightMetadata(14));
+            _sourceGraph.AddEdge(vertex2, vertex3, CreateWeightMetadata(10));
+            _sourceGraph.AddEdge(vertex2, vertex4, CreateWeightMetadata(15));
+            _sourceGraph.AddEdge(vertex3, vertex4, CreateWeightMetadata(11));
+            _sourceGraph.AddEdge(vertex3, vertex6, CreateWeightMetadata(2));
+            _sourceGraph.AddEdge(vertex4, vertex5, CreateWeightMetadata(6));
+            _sourceGraph.AddEdge(vertex5, vertex6, CreateWeightMetadata(9));
+
+            // reverse edges
+            _sourceGraph.AddEdge(vertex2, vertex1, CreateWeightMetadata(7));
+            _sourceGraph.AddEdge(vertex3, vertex1, CreateWeightMetadata(9));
+            _sourceGraph.AddEdge(vertex6, vertex1, CreateWeightMetadata(14));
+            _sourceGraph.AddEdge(vertex3, vertex2, CreateWeightMetadata(10));
+            _sourceGraph.AddEdge(vertex4, vertex2, CreateWeightMetadata(15));
+            _sourceGraph.AddEdge(vertex4, vertex3, CreateWeightMetadata(11));
+            _sourceGraph.AddEdge(vertex6, vertex3, CreateWeightMetadata(2));
+            _sourceGraph.AddEdge(vertex5, vertex4, CreateWeightMetadata(6));
+            _sourceGraph.AddEdge(vertex6, vertex5, CreateWeightMetadata(9));
+
+            // source and target vertices
+            _sourceVertex = vertex1;
+            _targetVertex = vertex5;
+
+            // result graph
+            _resultGraph = Factory.GetInstance<GeometryGraphFactory>(_referenceSystemMock.Object).CreateNetwork();
+
+            // vertices
+            vertex1 = _resultGraph.AddVertex(new Coordinate(0, 0), CreateDistanceMetadata(0));
+            vertex2 = _resultGraph.AddVertex(new Coordinate(1, 0), CreateDistanceMetadata(7));
+            vertex3 = _resultGraph.AddVertex(new Coordinate(1, 1), CreateDistanceMetadata(9));
+            vertex4 = _resultGraph.AddVertex(new Coordinate(2, 1), CreateDistanceMetadata(20));
+            vertex5 = _resultGraph.AddVertex(new Coordinate(1, 2), CreateDistanceMetadata(20));
+            vertex6 = _resultGraph.AddVertex(new Coordinate(0, 2), CreateDistanceMetadata(11));
+
+            // edges
+            _resultGraph.AddEdge(vertex1, vertex2);
+            _resultGraph.AddEdge(vertex1, vertex3);
+            _resultGraph.AddEdge(vertex3, vertex4);
+            _resultGraph.AddEdge(vertex3, vertex6);
+            _resultGraph.AddEdge(vertex6, vertex5);
+        }
+
+        #endregion
+
+        #region Test methods
+
+        /// <summary>
+        /// Test case for operation execution.
+        /// </summary>
+        [Test]
+        public void DijkstrasSinglePathAlgorithmExecuteTest()
+        {
+            Func<IGraphEdge, Double> weightFunction = edge => Convert.ToDouble(edge["Weight"]);
+
+            IDictionary<OperationParameter, Object> parameters = new Dictionary<OperationParameter, Object>();
+            parameters[GraphOperationParameters.SourceVertex] = _sourceVertex;
+            parameters[GraphOperationParameters.TargetVertex] = _targetVertex;
+            parameters[GraphOperationParameters.WeightMetric] = weightFunction;
+
+            DijkstrasAlgorithm operation = new DijkstrasAlgorithm(_sourceGraph, parameters);
+
+            operation.Execute();
+
+            Assert.AreEqual(_resultGraph.VertexCount, operation.Result.VertexCount);
+            Assert.AreEqual(_resultGraph.EdgeCount, operation.Result.EdgeCount);
+
+            foreach (IGraphVertex resultVertex in operation.Result.Vertices)
+            {
+                IGraphVertex vertex = _resultGraph.GetVertex(resultVertex.Coordinate);
+
+                Assert.IsNotNull(vertex);
+                Assert.AreEqual(vertex["Distance"], resultVertex["Distance"]);
+
+                Assert.IsTrue(operation.Result.OutEdges(resultVertex).All(edge => _resultGraph.GetAllEdges(edge.Source.Coordinate, edge.Target.Coordinate).Count == 1));
+            }
+        }
+
+        #endregion
+
+        #region  Private methods
+
+        /// <summary>
+        /// Creates metadata containing a weight value.
+        /// </summary>
+        /// <param name="weight">The weight.</param>
+        /// <returns>The produced metadata.</returns>
+        private IDictionary<String, Object> CreateWeightMetadata(Double weight)
+        {
+            IDictionary<String, Object> metadata = new Dictionary<String, Object>();
+            metadata["Weight"] = weight;
+
+            return metadata;
+        }
+
+        /// <summary>
+        /// Creates metadata containing a distance value.
+        /// </summary>
+        /// <param name="distance">The distance.</param>
+        /// <returns>The produced metadata.</returns>
+        private IDictionary<String, Object> CreateDistanceMetadata(Double distance)
+        {
+            IDictionary<String, Object> metadata = new Dictionary<String, Object>();
+            metadata["Distance"] = distance;
+
+            return metadata;
+        }
+
+        #endregion
+    }
+}
