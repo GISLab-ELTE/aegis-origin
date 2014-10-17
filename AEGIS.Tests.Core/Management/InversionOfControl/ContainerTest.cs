b9add4a60a163711066d88ef7b0ac892a5c0eb36
groberto <groberto@inf.elte.hu>
2014-10-17 09:39:20 +0200

    AEGIS.Tests.Core/Management/InversionOfControl: Fixed construction of geometry factory in ContainerTest.

--- AEGIS.Tests.Core/Management/InversionOfControl/ContainerTest.cs
+++ AEGIS.Tests.Core/Management/InversionOfControl/ContainerTest.cs
@@ -3,7 +3,7 @@
 ///     Educational Community License, Version 2.0 (the "License"); you may
 ///     not use this file except in compliance with the License. You may
 ///     obtain a copy of the License at
-///     http://www.osedu.org/licenses/ECL-2.0
+///     http://opensource.org/licenses/ECL-2.0
 ///
 ///     Unless required by applicable law or agreed to in writing,
 ///     software distributed under the License is distributed on an "AS IS"
@@ -96,7 +96,7 @@ namespace ELTE.AEGIS.Tests.Management.InversionOfControl
         [TestCase]
         public void ContainerResolveInstanceTest()
         {
-            IGeometryFactory factory = new GeometryFactory(null);
+            IGeometryFactory factory = Factory.DefaultInstance<IGeometryFactory>();
 
             // test case 1: resolve unregistered instance
 
@@ -160,9 +160,9 @@ namespace ELTE.AEGIS.Tests.Management.InversionOfControl
             Assert.IsFalse(_container.TryResolveInstance<IGeometryFactory>(out resolvedGeometryFactory));
             Assert.IsNull(resolvedGeometryFactory);
 
-            // test case 2: resolve registeres instance
+            // test case 2: resolve registered instance
 
-            IGeometryFactory geometryfactory = new GeometryFactory(null);
+            IGeometryFactory geometryfactory = Factory.DefaultInstance<IGeometryFactory>();
 
             _container.RegisterInstance<IGeometryFactory>(geometryfactory);
 
