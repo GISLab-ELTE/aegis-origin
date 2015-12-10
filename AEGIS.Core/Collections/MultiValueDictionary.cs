1c922aba94548758a27bde5d5c6d8e7d5ce6fc52
Roberto Giachetta <groberto@inf.elte.hu>
2015-12-10 16:18:08 +0100

    AEGIS.Core/Collections: Added multi-value dictionary (including unit tests).

---
+++ AEGIS.Core/Collections/MultiValueDictionary.cs
@@ -0,0 +1,329 @@
+﻿/// <copyright file="MultiValueDictionary.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2015 Roberto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Daniel Ballagi</author>
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ELTE.AEGIS.Collections
+{
+    /// <summary>
+    /// Represents a multi-value dictionary.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    public class MultiValueDictionary<TKey, TValue> : IDictionary<TKey, ICollection<TValue>>
+    {
+        #region Private fields
+
+        /// <summary>
+        /// Underlying dictionary.
+        /// </summary>
+        private Dictionary<TKey, List<TValue>> _dictionary;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiValueDictionary{TKey, TValue}" /> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.
+        /// </summary>
+        public MultiValueDictionary()
+        {
+            _dictionary = new Dictionary<TKey, List<TValue>>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiValueDictionary{TKey, TValue}" /> class that is empty, has the default initial capacity, and uses the specified <see cref="IComparer{TKey}" />.
+        /// </summary>
+        /// <param name="comparer">The <see cref="IComparer{T}" /> implementation to use when comparing keys, or null to use the default <see cref="Comparer{TKey}" /> for the type of the key.</param>
+        public MultiValueDictionary(IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, List<TValue>>(comparer);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiValueDictionary{TKey, TValue}" /> class that contains elements copied from the specified <see cref="MultiValueDictionary{TKey, TValue}" /> and uses the default equality comparer for the key type.
+        /// </summary>
+        /// <param name="other">The <see cref="MultiValueDictionary{TKey, TValue}" /> whose elements are copied to the new <see cref="MultiValueDictionary{TKey, TValue}" />.</param>
+        public MultiValueDictionary(MultiValueDictionary<TKey, TValue> other)
+        {
+            _dictionary = new Dictionary<TKey,List<TValue>>(other._dictionary);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiValueDictionary{TKey, TValue}" /> class that contains elements copied from the specified <see cref="MultiValueDictionary{TKey, TValue}" /> and uses the specified <see cref="IComparer{TKey}" />.
+        /// </summary>
+        /// <param name="other">The <see cref="MultiValueDictionary{TKey, TValue}" /> whose elements are copied to the new <see cref="MultiValueDictionary{TKey, TValue}" />.</param>
+        /// /// <param name="comparer">The <see cref="IComparer{T}" /> implementation to use when comparing keys, or null to use the default <see cref="Comparer{TKey}" /> for the type of the key.</param>
+        public MultiValueDictionary(MultiValueDictionary<TKey, TValue> other, IEqualityComparer<TKey> comparer)
+        {
+            _dictionary = new Dictionary<TKey, List<TValue>>(other._dictionary, comparer);
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Adds the specified key and value to the dictionary.
+        /// </summary>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="value">The value of the element to add. The value can be null for reference types.</param>
+        public void Add(TKey key, TValue value)
+        {
+            if (!_dictionary.ContainsKey(key))
+                _dictionary[key] = new List<TValue>();
+
+            _dictionary[key].Add(value);
+        }
+
+        /// <summary>
+        /// Removes the value with the specified key from the dictionary.
+        /// </summary>
+        /// <param name="key">The key of the element to remove.</param>
+        /// <returns><c>true</c> if the element is successfully found and removed; otherwise, <c>false</c>. This method returns <c>false</c> if key is not found.</returns>
+        public Boolean Remove(TKey key, TValue item)
+        {
+            if (!ContainsKey(key))
+                return false;
+
+            if (!_dictionary[key].Contains(item))
+                return false;
+
+            _dictionary[key].Remove(item);
+
+            if (_dictionary[key].Count() == 0)
+                _dictionary.Remove(key);
+
+            return true;
+        }
+
+        #endregion
+
+        #region ICollection properties
+
+        /// <summary>
+        /// Gets the number of elements contained in the dictionary.
+        /// </summary>
+        public Int32 Count
+        {
+            get { return _dictionary.Count(); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the collection is read-only.
+        /// </summary>
+        public Boolean IsReadOnly
+        {
+            get { return false; }
+        }
+
+        #endregion
+
+        #region IDictionary properties
+
+        /// <summary>
+        /// Gets the keys currently present in the dictionary.
+        /// </summary>
+        public ICollection<TKey> Keys
+        {
+            get { return _dictionary.Keys; }
+        }
+
+        /// <summary>
+        /// Gets the values currently present in the dictionary.
+        /// </summary>
+        public ICollection<ICollection<TValue>> Values
+        {
+            get { return _dictionary.Values.ToList<ICollection<TValue>>(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the element at the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>The value located at <paramref name="key" />.</returns>
+        public ICollection<TValue> this[TKey key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value.ToList<TValue>(); }
+        }
+
+        #endregion
+
+        #region ICollection methods
+
+        /// <summary>
+        /// Removes all items from the collection.
+        /// </summary>
+        public void Clear()
+        {
+            _dictionary.Clear();
+        }
+
+        /// <summary>
+        /// Adds a KeyValuePair to the dictionary.
+        /// </summary>
+        /// <param name="item">The object to add to the dictionary.</param>
+        void ICollection<KeyValuePair<TKey, ICollection<TValue>>>.Add(KeyValuePair<TKey, ICollection<TValue>> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        /// <summary>
+        /// Determines whether the dictionary contains a collection of values.
+        /// </summary>
+        /// <returns><c>true</c> if all values of <paramref name="item"/> are found in the dictionary; otherwise, <c>false</c>.</returns>
+        /// <param name="item">The collection of values to locate in the dictionary.</param>
+        Boolean ICollection<KeyValuePair<TKey, ICollection<TValue>>>.Contains(KeyValuePair<TKey, ICollection<TValue>> item)
+        {
+            if (!ContainsKey(item.Key))
+                return false;
+
+            foreach (TValue it in item.Value)
+            {
+                if (!_dictionary[item.Key].Contains(it))
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Copies the elements of the dictionary to an array, starting at a particular array index.
+        /// </summary>
+        /// <param name="array">The one-dimensional array that is the destination of the elements copied from dictionary. The array must have zero-based indexing.</param>
+        /// <param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins.</param>
+        void ICollection<KeyValuePair<TKey, ICollection<TValue>>>.CopyTo(KeyValuePair<TKey, ICollection<TValue>>[] array, int index)
+        {
+            if (index < 0)
+                return;
+
+            KeyValuePair<TKey, List<TValue>>[] tempArray = new KeyValuePair<TKey,List<TValue>>[_dictionary.Count()];
+            (_dictionary as IDictionary<TKey, List<TValue>>).CopyTo(tempArray, 0);
+
+            for (int i = index; i < tempArray.Length; ++i)
+            {
+                array[i - index] = new KeyValuePair<TKey, ICollection<TValue>>(tempArray[i].Key, tempArray[i].Value as ICollection<TValue>);
+            }
+        }
+
+        /// <summary>
+        /// Removes a specific KeyValuePair from the dictionary.
+        /// </summary>
+        /// <returns><c>true</c> if all values of <paramref name="item"/> were successfully removed from the dictionary; otherwise, <c>false</c>.</returns>
+        Boolean ICollection<KeyValuePair<TKey, ICollection<TValue>>>.Remove(KeyValuePair<TKey, ICollection<TValue>> item)
+        {
+            if (!ContainsKey(item.Key))
+                return false;
+
+            foreach (TValue it in item.Value)
+            {
+                if (!this[item.Key].Contains(it))
+                    return false;
+            }
+
+            Remove(item.Key);
+
+            return true;
+        }
+
+        #endregion
+
+        #region IDictionary methods
+
+        /// <summary>
+        /// Adds the specified key and value to the dictionary.
+        /// </summary>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="value">The value of the element to add. The value can be null for reference types.</param>
+        public void Add(TKey key, ICollection<TValue> value)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                _dictionary[key].AddRange(value);
+            }
+            else
+            {
+                _dictionary[key] = value.ToList<TValue>();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the dictionary contains the specified key.
+        /// </summary>
+        /// <param name="key">The key to locate.</param>
+        /// <returns><c>true</c> if <paramref name="key"/> is found in the dictionary; otherwise, <c>false</c>.</returns>
+        public Boolean ContainsKey(TKey key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Removes the value with the specified key from the dictionary.
+        /// </summary>
+        /// <param name="key">The key of the element to remove.</param>
+        /// <returns><c>true</c> if the element is successfully found and removed; otherwise, <c>false</c>. This method returns <c>false</c> if key is not found.</returns>
+        public Boolean Remove(TKey key) 
+        {
+            return _dictionary.Remove(key);
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the value to get.</param>
+        /// <param name="value">When this method returns, contains the value associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter. This parameter is passed uninitialized.</param>
+        /// <returns><c>true</c> if the dictionary contains an element with the specified key; otherwise, <c>false</c>.</returns>
+        public Boolean TryGetValue(TKey key, out ICollection<TValue> value)
+        {
+            List<TValue> outValue;
+            Boolean retValue = _dictionary.TryGetValue(key, out outValue);
+
+            value = outValue;
+
+            return retValue;
+        }
+
+        #endregion
+
+        #region IEnumerable methods
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{KeyValuePair{TKey, TValue}}" /> object that can be used to iterate through the collection.</returns>
+        public IEnumerator<KeyValuePair<TKey, ICollection<TValue>>> GetEnumerator()
+        {
+            foreach (KeyValuePair<TKey, ICollection<TValue>> item in _dictionary as IDictionary<TKey, ICollection<TValue>>)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator" /> object that can be used to iterate through the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
---
+++ AEGIS.Tests.Core/Collections/MultiValueDictionaryTest.cs
@@ -0,0 +1,243 @@
+﻿/// <copyright file="MultiValueDictionaryTest.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2015 Roberto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Daniel Ballagi</author>
+
+using ELTE.AEGIS.Collections;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ELTE.AEGIS.Tests.Collections
+{
+    /// <summary>
+    /// Test fixture for the <see cref="MultiValueDictionary" /> class.
+    /// </summary>
+    [TestFixture]
+    class MultiValueDictionaryTest
+    {
+        #region Private fields
+
+        /// <summary>
+        /// The array of values that are inserted into the dictionary.
+        /// </summary>
+        private KeyValuePair<Int32, List<String>>[] _items;
+
+        #endregion
+
+        #region Test setup
+
+        /// <summary>
+        /// Test setup.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            _items = Enumerable.Range(1, 10).Select(value => new KeyValuePair<Int32, List<String>>(value, new List<String>() 
+            { 
+                value.ToString(),
+                value.ToString() + value.ToString()
+            })).ToArray();
+        }
+
+        #endregion
+
+        #region Test methods
+
+        /// <summary>
+        /// Tests the constructors.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryConstructorTest()
+        {
+            // No parameters
+
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+            Assert.AreEqual(0, dictionary.Count());
+            Assert.AreEqual(false, dictionary.IsReadOnly);
+
+            // Comparer parameter
+
+            dictionary = new MultiValueDictionary<Int32, String>(EqualityComparer<Int32>.Default);
+            Assert.AreEqual(0, dictionary.Count());
+            Assert.AreEqual(false, dictionary.IsReadOnly);
+
+            // Copy constructor
+
+            dictionary.Add(1, "one");
+            MultiValueDictionary<Int32, String> secondDictionary = new MultiValueDictionary<Int32, String>(dictionary);
+            Assert.AreEqual(1, secondDictionary.Count);
+            Assert.AreEqual(new List<String> { "one" }, secondDictionary[1]);
+            Assert.AreEqual(false, dictionary.IsReadOnly);
+
+            // Copy constructor with comparer parameter
+
+            secondDictionary = new MultiValueDictionary<Int32, String>(dictionary, EqualityComparer<Int32>.Default);
+            Assert.AreEqual(1, secondDictionary.Count);
+            Assert.AreEqual(new List<String> { "one" }, secondDictionary[1]);
+            Assert.AreEqual(false, dictionary.IsReadOnly);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Add" /> method.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryAddTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+            dictionary.Add(1, "1");
+            Assert.AreEqual(1, dictionary.Count);
+            Assert.AreEqual(new List<String> { "1" }, dictionary[1]);
+
+            dictionary.Add(1, "11");
+            Assert.AreEqual(1, dictionary.Count);
+            Assert.AreEqual(new List<String> { "1", "11" }, dictionary[1]);
+
+            dictionary.Add(2, new List<String> { "2", "22" });
+            Assert.AreEqual(2, dictionary.Count);
+            Assert.AreEqual(new List<String> { "2", "22" }, dictionary[2]);
+
+            dictionary.Add(2, new List<String> { "222", "2222" });
+            Assert.AreEqual(2, dictionary.Count);
+            Assert.AreEqual(new List<String> { "2", "22", "222", "2222" }, dictionary[2]);
+
+            (dictionary as ICollection<KeyValuePair<Int32, ICollection<String>>>).Add(new KeyValuePair<Int32, ICollection<String>>(3, new List<String> { "3" } as ICollection<String>));
+            Assert.AreEqual(3, dictionary.Count);
+            Assert.AreEqual(new List<String> { "3" }, dictionary[3]);
+
+            dictionary[4] = new List<String> { "4" };
+            Assert.AreEqual(4, dictionary.Count);
+            Assert.AreEqual(new List<String> { "4" }, dictionary[4]);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Remove" /> method.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryRemoveTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            Assert.AreEqual(false, dictionary.Remove(0, "0"));
+            Assert.AreEqual(true, dictionary.Remove(1, "1"));
+            Assert.AreEqual(false, dictionary.Remove(1, "1"));
+            Assert.AreEqual(true, dictionary.Remove(1, "11"));
+            Assert.AreEqual(false, dictionary.Remove(1, "11"));
+
+            dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            Assert.AreEqual(false, dictionary.Remove(0));
+            Assert.AreEqual(true, dictionary.Remove(1));
+            Assert.AreEqual(false, dictionary.Remove(1));
+
+            (dictionary as ICollection<KeyValuePair<Int32, ICollection<String>>>).Add(new KeyValuePair<Int32, ICollection<String>>(3, new List<String> { "3" } as ICollection<String>));
+            Assert.AreEqual(true, (dictionary as ICollection<KeyValuePair<Int32, ICollection<String>>>).Remove(new KeyValuePair<Int32, ICollection<String>>(3, new List<String> { "3" } as ICollection<String>)));
+            Assert.AreEqual(false, (dictionary as ICollection<KeyValuePair<Int32, ICollection<String>>>).Remove(new KeyValuePair<Int32, ICollection<String>>(3, new List<String> { "3" } as ICollection<String>)));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Clear" /> method.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryClearTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            Assert.AreEqual(10, dictionary.Count());
+
+            dictionary.Clear();
+            Assert.AreEqual(0, dictionary.Count());
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Contains" /> method.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryContainsTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            Assert.AreEqual(true, dictionary.ContainsKey(1));
+            Assert.AreEqual(false, dictionary.ContainsKey(0));
+
+            Assert.AreEqual(true, dictionary.Contains(new KeyValuePair<Int32, ICollection<String>>(1, new List<String> { "1", "11" } as ICollection<String>)));
+            Assert.AreEqual(false, dictionary.Contains(new KeyValuePair<Int32, ICollection<String>>(1, new List<String> { "0", "11" } as ICollection<String>)));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="CopyTo" /> method.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryCopyToTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            KeyValuePair<Int32, ICollection<String>>[] actual = new KeyValuePair<Int32,ICollection<String>>[10];
+            (dictionary as ICollection<KeyValuePair<Int32, ICollection<String>>>).CopyTo(actual, 0);
+
+            IEnumerable<KeyValuePair<Int32, ICollection<String>>> expected = _items.Select(value => new KeyValuePair<Int32, ICollection<String>>(value.Key, value.Value as ICollection<String>));
+
+            Assert.AreEqual(expected.Select(v => v.Key), actual.Select(v => v.Key));
+            Assert.AreEqual(expected.Select(v => v.Value), actual.Select(v => v.Value));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Keys" /> property.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryKeysTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            Assert.IsTrue(dictionary.Keys.SequenceEqual(Enumerable.Range(1, 10)));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Values" /> property.
+        /// </summary>
+        [Test]
+        public void MultiValueDictionaryValuesTest()
+        {
+            MultiValueDictionary<Int32, String> dictionary = new MultiValueDictionary<Int32, String>();
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                dictionary.Add(item.Key, item.Value);
+
+            Assert.AreEqual(_items.Length, dictionary.Values.Count);
+
+            foreach (KeyValuePair<Int32, List<String>> item in _items)
+                Assert.IsTrue(dictionary.Values.Any(value => value.SequenceEqual(item.Value)));
+        }
+
+        #endregion
+    }
+}
