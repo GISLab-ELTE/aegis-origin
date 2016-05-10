6096d8e049948cb1eb072ba00f733fe0604303a0
Roberto Giachetta <groberto@inf.elte.hu>
2016-05-10 10:11:05 +0200

    Core.Spectral/Collections: Added raster section and section map to be used in partitioning/tiling.

---
+++ AEGIS.Core.Spectral/Collections/RasterSection.cs
@@ -0,0 +1,164 @@
+﻿/// <copyright file="RasterSection.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2016 Roberto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Roberto Giachetta</author>
+
+using System;
+
+namespace ELTE.AEGIS.Collections
+{
+    /// <summary>
+    /// Represents a raster tile.
+    /// </summary>
+    public class RasterSection
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RasterSection" /> class.
+        /// </summary>
+        public RasterSection()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RasterSection" /> class.
+        /// </summary>
+        /// <param name="rowIndex">The row index.</param>
+        /// <param name="columnIndex">The column index.</param>
+        /// <param name="numberOfRows">The number of rows.</param>
+        /// <param name="numberOfColumns">The number of columns.</param>
+        public RasterSection(Int32 rowIndex, Int32 columnIndex, Int32 numberOfRows, Int32 numberOfColumns)
+            : this (0, rowIndex, columnIndex, numberOfRows, numberOfColumns)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RasterSection" /> class.
+        /// </summary>
+        /// <param name="id">The part identifier.</param>
+        /// <param name="rowIndex">The row index.</param>
+        /// <param name="columnIndex">The column index.</param>
+        /// <param name="numberOfRows">The number of rows.</param>
+        /// <param name="numberOfColumns">The number of columns.</param>
+        public RasterSection(Int32 id, Int32 rowIndex, Int32 columnIndex, Int32 numberOfRows, Int32 numberOfColumns)
+        {
+            Id = id;
+            RowIndex = rowIndex;
+            ColumnIndex = columnIndex;
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+        }
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Gets the tile identifier.
+        /// </summary>
+        /// <value>
+        /// The tile identifier.
+        /// </value>
+        public Int32 Id { get; set; }
+
+        /// <summary>
+        /// Gets thw index of the first row within the section.
+        /// </summary>
+        /// <value>The index of the first row within the section.</value>
+        public Int32 RowIndex { get; set; }
+
+        /// <summary>
+        /// Gets thw index of the first row after the section.
+        /// </summary>
+        /// <value>The index of the first row after the section.</value>
+        public Int32 RowEndIndex { get { return RowIndex + NumberOfRows; } }
+
+        /// <summary>
+        /// Gets the index of the first column within the section.
+        /// </summary>
+        /// <value>The index of the first column within the section.</value>
+        public Int32 ColumnIndex { get; set; }
+
+        /// <summary>
+        /// Gets the index of the first column after the section.
+        /// </summary>
+        /// <value>The index of the first column after the section.</value>
+        public Int32 ColumnEndIndex { get { return ColumnIndex + NumberOfColumns; } }
+
+        /// <summary>
+        /// Gets the number of rows in the section.
+        /// </summary>
+        /// <value>
+        /// The number of rows in the section.
+        /// </value>
+        public Int32 NumberOfRows { get; set; }
+
+        /// <summary>
+        /// Gets the number of columns in the section.
+        /// </summary>
+        /// <value>The number of columns in the section.</value>
+        public Int32 NumberOfColumns { get; set; }
+
+        /// <summary>
+        /// Gets the area of the raster.
+        /// </summary>
+        /// <value>The number of spectral values within the raster.</value>
+        public Int32 Area { get { return NumberOfRows * NumberOfColumns; } }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Determines whether this section matches another.
+        /// </summary>
+        /// <param name="other">The other section.</param>
+        /// <returns><c>true</c> if this instance has the same dimensions as <paramref name="other"/>; otherwise, <c>false</c>.</returns>
+        public Boolean IsMatching(RasterSection other)
+        {
+            return RowIndex == other.RowIndex &&
+                   ColumnIndex == other.ColumnIndex &&
+                   NumberOfRows == other.NumberOfRows &&
+                   NumberOfColumns == other.NumberOfColumns;
+        }
+
+        /// <summary>
+        /// Determines whether this section contains another.
+        /// </summary>
+        /// <param name="other">The other section.</param>
+        /// <returns><c>true</c> if this instance contains <paramref name="other"/>; otherwise, <c>false</c>.</returns>
+        public Boolean Contains(RasterSection other)
+        {
+            return RowIndex <= other.RowIndex &&
+                   ColumnIndex <= other.ColumnIndex &&
+                   RowIndex + NumberOfRows >= other.RowIndex + other.NumberOfRows &&
+                   ColumnIndex + NumberOfColumns >= other.ColumnIndex + other.NumberOfColumns;
+        }
+
+        #endregion
+
+        #region Object methods
+
+        /// <summary>
+        /// Returns a <see cref="String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="String" /> that represents this instance.</returns>
+        public override String ToString()
+        {
+            return String.Format("{0},{1},{2},{3},{4}", Id, RowIndex, ColumnIndex, NumberOfRows, NumberOfColumns);
+        }
+
+        #endregion
+    }
+}
---
+++ AEGIS.Core.Spectral/Collections/RasterSectionMap.cs
@@ -0,0 +1,188 @@
+﻿/// <copyright file="RasterSectionMap.cs" company="Eötvös Loránd University (ELTE)">
+///     Copyright (c) 2011-2016 Roberto Giachetta. Licensed under the
+///     Educational Community License, Version 2.0 (the "License"); you may
+///     not use this file except in compliance with the License. You may
+///     obtain a copy of the License at
+///     http://opensource.org/licenses/ECL-2.0
+///
+///     Unless required by applicable law or agreed to in writing,
+///     software distributed under the License is distributed on an "AS IS"
+///     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+///     or implied. See the License for the specific language governing
+///     permissions and limitations under the License.
+/// </copyright>
+/// <author>Roberto Giachetta</author>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELTE.AEGIS.Collections
+{
+    /// <summary>
+    /// Represents a type for mapping raster section to raster tiles.
+    /// </summary>
+    public class RasterSectionMap
+    {
+        #region Private fields
+
+        /// <summary>
+        /// The list of sections.
+        /// </summary>
+        private List<RasterSection> _sections;
+
+        /// <summary>
+        /// The list of tile sections.
+        /// </summary>
+        private List<RasterSection> _tileSections;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RasterSectionMap" /> class.
+        /// </summary>
+        public RasterSectionMap()
+        {
+            _sections = new List<RasterSection>();
+            _tileSections = new List<RasterSection>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RasterSectionMap" /> class.
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <exception cref="System.ArgumentNullException">The other section map is null.</exception>
+        public RasterSectionMap(RasterSectionMap other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other", "The other section map is null.");
+
+            _sections = new List<RasterSection>(other.Sections);
+            _tileSections = new List<RasterSection>(other.TileSections);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RasterSectionMap" /> class.
+        /// </summary>
+        /// <param name="sections">The collection of raster sections.</param>
+        /// <param name="tileSections">The collection of tile sections.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// The section collection is null.
+        /// or
+        /// The tile section collection is null.
+        /// </exception>
+        public RasterSectionMap(IEnumerable<RasterSection> sections, IEnumerable<RasterSection> tileSections)
+        {
+            if (sections == null)
+                throw new ArgumentNullException("sections", "The section collection is null.");
+            if (tileSections == null)
+                throw new ArgumentNullException("tileSections", "The tile section collection is null.");
+
+            _sections = new List<RasterSection>(sections);
+            _tileSections = new List<RasterSection>(tileSections);
+        }
+
+        #endregion
+
+        #region Public properties
+        
+        /// <summary>
+        /// Gets the sections of the raster.
+        /// </summary>
+        /// <value>The list of sections.</value>
+        public IReadOnlyList<RasterSection> Sections { get { return _sections; } }
+
+        /// <summary>
+        /// Gets the sections of the tile.
+        /// </summary>
+        /// <value>The list of tile sections.</value>
+        public IReadOnlyList<RasterSection> TileSections { get { return _tileSections; } }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Adds the specified section to the map.
+        /// </summary>
+        /// <param name="section">The section.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">The section is null.</exception>
+        public Int32 AddSection(RasterSection section)
+        {
+            if (section == null)
+                throw new ArgumentNullException("section", "The section is null.");
+
+            if (section.RowIndex < 0 ||section.ColumnIndex < 0 || section.Area == 0)
+                return 0;
+
+            foreach (RasterSection existingSection in _sections)
+                if (existingSection.IsMatching(section))
+                {
+                    section.Id = existingSection.Id;
+                    return section.Id;
+                }
+
+            section.Id = _sections.Count + 1;
+            _sections.Add(section);
+            return section.Id;
+        }
+
+        /// <summary>
+        /// Adds the specified tile section to the map.
+        /// </summary>
+        /// <param name="section">The section.</param>
+        /// <exception cref="System.ArgumentNullException">The section is null.</exception>
+        public void AddTileSection(RasterSection section)
+        {
+            if (section == null)
+                throw new ArgumentNullException("section", "The section is null.");
+
+
+
+            if (section.Id == 0 || section.RowIndex < 0 || section.ColumnIndex < 0 || section.Area == 0)
+                return;
+
+            foreach (RasterSection existingSection in _tileSections)
+                if (existingSection.IsMatching(section))
+                    return;
+
+            _tileSections.Add(section);
+        }
+
+        #endregion
+
+        #region Object methods
+        
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder("[");
+            for (Int32 index = 0; index < _sections.Count; index++)
+            {
+                if (index > 0)
+                    builder.Append('|');
+
+                builder.Append(_sections[index].ToString());
+            }
+            builder.Append("][");
+            for (Int32 index = 0; index < _sections.Count; index++)
+            {
+                if (index > 0)
+                    builder.Append('|');
+
+                builder.Append(_sections[index].ToString());
+            }
+            builder.Append("]");
+
+            return builder.ToString();
+        }
+
+        #endregion        
+    }
+}
