a82a477c26b45ee115b83265deafc3722da491c3
daniel-rck <25479086+daniel-rck@users.noreply.github.com>
2023-03-21 11:48:43 +0100

    Update AEGIS.Tests.Core.Reference/Operations/Projections/LambertAzimuthalEqualAreaProjectionTest.cs
    Co-authored-by: Máté Cserép <mcserep@gmail.com>

--- AEGIS.Tests.Core.Reference/Operations/Projections/LambertAzimuthalEqualAreaProjectionTest.cs
+++ AEGIS.Tests.Core.Reference/Operations/Projections/LambertAzimuthalEqualAreaProjectionTest.cs
@@ -1,4 +1,4 @@
-﻿/// <copyright file="LabordeObliqueMercatorProjectionTest.cs" company="Eötvös Loránd University (ELTE)">
+/// <copyright file="LambertAzimuthalEqualAreaProjectionTest.cs" company="Eötvös Loránd University (ELTE)">
 ///     Copyright (c) 2011-2022 Roberto Giachetta. Licensed under the
 ///     Educational Community License, Version 2.0 (the "License"); you may
 ///     not use this file except in compliance with the License. You may
